7fbf68f6c81b352c2b215d8d5c31442751c00b89
Prashant Srivastava <82441036+prashantmagnifiedcode@users.noreply.github.com>
2022-01-22 23:13:52 +0530

    Updates

---
+++ precache-manifest.41f7a68edacf0bc8ea728d76787bc3b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3285f0326b6d880f6027bd5c7705f6f9",
+    "url": "/Election-Dapp/index.html"
+  },
+  {
+    "revision": "0b50e36617179065cd1f",
+    "url": "/Election-Dapp/static/css/main.d8f1efea.chunk.css"
+  },
+  {
+    "revision": "928f5650e011476a1d35",
+    "url": "/Election-Dapp/static/js/2.924cdcf4.chunk.js"
+  },
+  {
+    "revision": "0b50e36617179065cd1f",
+    "url": "/Election-Dapp/static/js/main.109145a8.chunk.js"
+  },
+  {
+    "revision": "168bdf195ea6aba35517",
+    "url": "/Election-Dapp/static/js/runtime-main.05697d3e.js"
+  }
+]);
